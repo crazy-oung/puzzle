bcdc3384a8faf60590369efe44be8e3acd0f2492
crazy-oung <d_yng@naver.com>
2019-10-22 19:58:04 +0900

    회원 탈퇴 버그 수정, 탈퇴 버튼 클릭시 비밀번호 입력 확인 절차 추가

--- puzzle/src/model/MemberDao.java
+++ puzzle/src/model/MemberDao.java
@@ -4,9 +4,11 @@ import java.sql.*;
 
 public class MemberDao {
 	
+	// 회원 탈퇴 - 회원 비 활성화 
 	public void withdrawMember(Connection conn, Member member)throws Exception {
+		System.out.println("::: withdrawMember@MemberDAO 실행::: ");
 	      PreparedStatement stmt = null;
-	      String sql = "UPDATE member SET member_level = 'N' WHERE member_id = ? AND member_pw = ?;";
+	      String sql = "UPDATE member SET level = 'N' WHERE member_id = ? AND member_pw = ?;";
 	      try {
 	         stmt = conn.prepareStatement(sql);
 	         stmt.setString(1, member.getMemberId());
@@ -20,8 +22,9 @@ public class MemberDao {
 	            }
 	         }
 	   }
-	
+	// 회원 정보 수정
 	public String updateMemberInfo(Connection conn, Member member, String newPw) {
+		System.out.println("::: updateMemberInfo@MemberDAO 실행::: ");
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 		String sql = null;
@@ -38,7 +41,8 @@ public class MemberDao {
 			// 기존 비밀번호가 틀렸을 때 
 			if(!pw.equals(member.getMemberPw())) {				
 				return "비밀번호가 틀렸습니다. 기존 비밀번호를 정확히 입력해주세요!";
-			}		
+			}
+			//기존 비밀번호와 변경 비밀번호가 같을 때 
 			if(pw.equals(newPw)) {				
 				return "수정을 요청하신 비밀번호가 기존 비밀번호와 동일 합니다.";
 			}	
@@ -62,7 +66,9 @@ public class MemberDao {
 		return "정보 수정 완료 ! :)";
 	}
 	
+	// 회원 정보 조회
 	public Member SelectMemberInfo(Connection conn, Member member) {
+		System.out.println("::: SelectMemberInfo@MemberDAO 실행::: ");
 		Member mem = new Member();
 		// 비밀번호는 함부로 추출하지 않음.
 		String sql = "SELECT member_id FROM member WHERE member_id =?";
@@ -88,7 +94,9 @@ public class MemberDao {
 		return mem;
 	}
 	
+	// 로그인 - 회원 조회
 	public Member login(Connection conn, Member member) {
+		System.out.println("::: login@MemberDAO 실행::: ");
 		Member mem = new Member();
 		String sql = "SELECT member_id FROM member WHERE member_id =? AND member_pw=? AND level = 'Y'";
 		PreparedStatement stmt = null;
@@ -114,8 +122,9 @@ public class MemberDao {
 		return mem;
 	}
 	
+	// 회원가입 - 회원 추가
 	public void addMember(Connection conn, Member member)throws Exception {
-		System.out.println("::: addMember 실행:::");
+		System.out.println("::: addMember@MemberDAO 실행::: ");
 		System.out.println(member.toString());
 	      PreparedStatement stmt = null;
 	      String sql = "INSERT INTO member(member_id, member_pw, level) VALUES (?, ?, 'Y')";
